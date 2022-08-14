05067dd5bb311ad0dba18bde4dd5119faefb427d
nohanaguib <67865887+nohanaguib@users.noreply.github.com>
2022-08-14 10:29:42 +0200

    Add files via upload

---
+++ CoffeeProject/cofee/cofee/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  cofee
+//
+//  Created by Mayar Adel on 8/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btnview: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        btnview.layer.cornerRadius = 30.5
+    }
+    
+
+
+}
+
---
+++ CoffeeProject/register-2/register/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  register
+//
+//  Created by Mayar Adel on 8/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Button: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Button.layer.cornerRadius = 25
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
